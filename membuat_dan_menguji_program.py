eac9f5d3967c96c0a690cd9a57b3fd2df433ca82
Pipin Fitriadi <pipinfitriadi@gmail.com>
2023-10-18 10:49:47 +0700

    fix: Add license to membuat_dan_menguji_program.py

--- membuat_dan_menguji_program.py
+++ membuat_dan_menguji_program.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Pipin Fitriadi. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for license
+# information.
+
 from datetime import date
 
 
