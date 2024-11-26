bd0aa48b4342c96db3bd774eabad573614571894
siti-yl <sitiyulianti0907@gmail.com>
2024-11-26 02:48:01 +0000

    UTS-Siti Yulianti-0426006

--- uts.py
+++ uts.py
@@ -1,5 +1,1 @@
-#!/usr/bin/env python3
-
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+print ("hello")
